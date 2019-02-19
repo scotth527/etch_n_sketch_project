4ad6544577b8043431b86ce093f2be8f98fdbf10
Scott Huang <scottyh527@gmail.com>
2019-02-19 16:17:30 +0000

    fixed bugs that would reset the grid and ask user to make a new grid

--- src/js/index.js
+++ src/js/index.js
@@ -3,38 +3,41 @@ import "bootstrap"; //breathecode dom for more explicit errors
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-let reset = document.querySelector("#reset");
-reset.addEventListener("click", () => {
-  let filled = document.querySelectorAll(".filledSquare");
-  let stuff = Array.from(filled);
-  for (let i = 0; i < stuff.length; i++) {
-    stuff[i].classList.toggle("filledSquare");
-  }
-});
-
 let totalWidth = 610;
 
 let determinedSize = input => {
   let singleWidth = totalWidth / input;
-
   return String(singleWidth) + "px";
 };
 
-let numberPerRow = prompt("How many squares per side would you like?");
+//
 
-if (typeof numberPerRow != "number") {
-  alert("Input incorrect");
+let reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
   let numberPerRow = prompt("How many squares per side would you like?");
-}
-
-let totalSquares = () => numberPerRow * numberPerRow;
-
-window.onload = function() {
+  let totalSquares = () => numberPerRow * numberPerRow;
   let sketch = document.querySelector("#sketch");
+  while (sketch.hasChildNodes()) {
+    sketch.removeChild(sketch.firstChild);
+  }
   for (var i = 0; i < totalSquares(); i++) {
     const div = document.createElement("div");
     div.style.width = `${determinedSize(numberPerRow)}`;
     div.style.height = `${determinedSize(numberPerRow)}`;
+    div.classList.add("emptySquare");
+    div.addEventListener("mouseover", e => {
+      e.target.classList.add("filledSquare");
+    });
+    sketch.appendChild(div);
+  }
+});
+
+window.onload = function setBoard() {
+  let sketch = document.querySelector("#sketch");
+  for (var i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${determinedSize(16)}`;
+    div.style.height = `${determinedSize(16)}`;
     div.classList.add("emptySquare");
     div.addEventListener("mouseover", e => {
       e.target.classList.add("filledSquare");
